7964a2219d27e6c79efd73db14049b7f3819498e
JoshDerr <89755697+JoshDerr@users.noreply.github.com>
2024-03-04 20:23:38 -0600

    Add files via upload

---
+++ app_config.php
@@ -0,0 +1,12 @@
+<?php
+//application folder name on your web root or htdocs folder 
+
+define('APP_ROOT', $_SERVER['DOCUMENT_ROOT']);
+
+//DEBUG echo (APP_ROOT).'<br>'; exit();
+
+define('APP_FOLDER_NAME', '/BIS4033/JoshDerrAssignment2');
+define('WEB_ROOT', 'http://'.$_SERVER['SERVER_NAME']);
+
+//DEBUG echo(WEB_ROOT).'<br>'; exit();
+?>
